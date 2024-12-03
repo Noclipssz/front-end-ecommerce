af8c71c8b335e2ea8c848a9cd8a66385e30f6557
_Kairo_ <kairo.h.seidler@gmail.com>
2024-12-02 21:41:03 -0300

    COMMIT

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const axios = require('axios');
 const path = require('path');
-const { atualizarProdutos } = require('./public/js/script');
 const app = express();
 
 app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.render('index'); 
+});
 
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
